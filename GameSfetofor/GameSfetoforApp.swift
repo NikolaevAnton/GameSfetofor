c8c1a03362173fbd0b5e5c85fc32d1a16fb82128
NikolaevAnton <mjop41@gmail.com>
2022-01-29 20:33:00 +0300

    Initial Commit

---
+++ GameSfetofor/GameSfetoforApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSfetoforApp.swift
+//  GameSfetofor
+//
+//  Created by Anton Nikolaev on 29.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameSfetoforApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
